d2e16690c6748a245e6435c41f00b5a5833da1d7
Somayeh Rahmani <rahmani.it27@gmail.com>
2024-11-11 13:03:23 +0330

    add navbar part

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let stays = document.getElementById("stay");
+let experience = document.getElementById("exper");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+stays?.addEventListener("click", () => {
+  stays.classList.add("Active");
+  stays.classList.remove("inactive");
+  experience?.classList.remove("Active");
+  experience?.classList.add("inactive");
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+experience?.addEventListener("click", () => {
+  experience.classList.add("Active");
+  experience.classList.remove("inactive");
+  stays?.classList.remove("Active");
+  stays?.classList.add("inactive");
+});
+
+console.log(stays);
