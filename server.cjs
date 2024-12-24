9236e10e151dab2e437638754227dc45491abb3b
Somayeh Rahmani <rahmani.it27@gmail.com>
2024-12-24 23:43:18 +0330

    add server express

---
+++ server.cjs
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.all("*", (req, res) => {
+  res.send("<h1>Resource not found</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
