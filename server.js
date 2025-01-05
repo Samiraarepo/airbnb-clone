e32930364d5f46ef9c968aba6a97c5af1aeb18fe
SamiraMiss <samirarostami71@gmail.com>
2025-01-06 01:41:22 +0330

    Added table.js and body-parser with sqlite3 .

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const viteExpress = require("vite-express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+const sqlite = require("sqlite3").verbose();
+const db = new sqlite.Database("./quote.db", sqlite.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err);
+});
+app.use(bodyParser.json());
 
 // Example route
 app.get("/api/hello", (req, res) => {
@@ -15,6 +22,19 @@ app.get("/api/greet", (req, res) => {
 // app.post("/api/data", (req, res) => {
 //   res.send("Data received!");
 // });
+
+// Post request
+app.post("/quote", (req, res) => {
+  try {
+    console.log(request.body.movie);
+    res.json({ status: 200, success: true });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      success: false,
+    });
+  }
+});
 viteExpress.listen(app, 3000, () => {
   console.log("Server is running with vite-express");
 });
