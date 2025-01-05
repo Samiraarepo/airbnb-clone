22b7aff0cecb30c201dfda544a7a54276725af32
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-01-05 11:47:25 +0330

    add some thing

--- server.cjs
+++ server.cjs
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use();
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
